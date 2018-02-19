aebe5a74d494fd17365479b5294026fce43fbe53
m4rthiz <m4rthiz@gmail.com>
2018-02-19 21:14:54 +0100

    fix navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,5 @@
 
-    <a class="navbar-brand" href="{{ URL::to('/') }}">Blog</a>
+    <a class="navbar-brand" href="{{ URL::to('/') }}">m4r</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,8 +13,8 @@
           <a class="nav-link" href="{{ URL::to('posts/create') }}">nouveau</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
+      {{-- <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      </form> --}}
     </div>
